2450847f9b6c9acc2152f57fc9137a3aabcf458b
Nikita Dubov <dubow1148@yandex.ru>
2023-10-15 11:09:58 +0300

    15.10.23 Стартовое меню + К вопросам

--- inline/inline.py
+++ inline/inline.py
@@ -1,9 +1,9 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-button_category1 = InlineKeyboardButton('Опция 1', callback_data='Опция 1')
-button_category2 = InlineKeyboardButton('Опция 2', callback_data='Опция 2')
-button_category3 = InlineKeyboardButton('Опция 3', callback_data='Опция 3')
-button_category4 = InlineKeyboardButton('Назад', callback_data='Назад')
+button_category1 = InlineKeyboardButton('К вопросам', callback_data='К вопросам')
+button_category2 = InlineKeyboardButton('К номерам', callback_data='К номерам')
+button_category3 = InlineKeyboardButton('К корпусам', callback_data='К корпусам')
+# button_category4 = InlineKeyboardButton('Назад', callback_data='Назад')
 
 keyboard = InlineKeyboardMarkup(row_width=1)
-keyboard.add(button_category1, button_category2, button_category3, button_category4)
+keyboard.add(button_category1, button_category2, button_category3)
--- main.py
+++ main.py
@@ -1,15 +1,47 @@
 from aiogram import types, executor
-from create_bot import dp
+from create_bot import dp, bot
 from inline import keyboard
+from aiogram.utils.markdown import *
 
 
 async def on_startup(_):
     print("Я запустился!")
 
 
+HELP_MESSAGE = """Данный бот является помощником для иностранных студентов
+В разделе 'К вопросам' Вы список самых часто задаваемых вопросов и ответов к ним
+В разделе 'К номерам' Вы можете найти список номеров, которые могут быть Вам полезны
+В разделе 'К корпусам' Вы можете найти информацию о корпусах (где находится, важные кабинеты и другое)
+Для начала работы с ботом пропишите команду /start
+Если у вас возникли вопросы или проблемы, свяжитесь с нами по электронной почте xxx@example.com"""
+
+my_list = HELP_MESSAGE.split('\n')
+
+result = ""
+for item in my_list:
+    result += f"\u2022 {item}\n"
+
+
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет! Вот тебе несколько вариантов", reply_markup=keyboard)
+    await message.reply("Привет! Выбирайте нужный Вам раздел", reply_markup=keyboard)
+
+
+@dp.message_handler(commands=['help'])
+async def process_start_command(message: types.Message):
+    await message.delete()
+    await message.answer(text=result)
+
+
+@dp.callback_query_handler(lambda x: x.data and x.data in 'К вопросам')
+async def QandA(callback_query: types.CallbackQuery):
+    questions = text(bold('Вопрос 1: '),
+                     '\nОтвет: ',
+                     bold('\n\nВопрос 2: '),
+                     '\nОтвет: ', )
+
+    await bot.send_message(callback_query.message.chat.id, questions, parse_mode="MARKDOWN")
+    await callback_query.answer()
 
 
 if __name__ == '__main__':
