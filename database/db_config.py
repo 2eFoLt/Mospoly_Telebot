15522c165852eaffd1f25519c41b124287efb357
ammarunov <90957477+ammarunov@users.noreply.github.com>
2024-02-18 20:16:55 +0300

    Add files via upload

--- database/db_config.py
+++ database/db_config.py
@@ -5,3 +5,11 @@ db_conf = {
     'port': '3306',
     'database': 'std_2074_pd'
 }
+
+db_conf_log = {
+    'user': 'std_2074_log',
+    'password': 'as12as12',
+    'host': 'std-mysql',
+    'port': '3306',
+    'database': 'std_2074_log'
+}
--- database/db_func.py
+++ database/db_func.py
@@ -1,13 +1,17 @@
-from .db_config import db_conf
+from .db_config import db_conf, db_conf_log
 import mysql.connector
 
 
 def sql_start():
-    global base, cur
+    global base, cur, base_log, cur_log
     base = mysql.connector.connect(**db_conf)
     cur = base.cursor()
+    base_log = mysql.connector.connect(**db_conf_log)
+    cur_log = base_log.cursor()
     if base:
         print('Database connected OK!')
+    if base_log:
+        print('Log Database connected OK!')
 
 
 def get_questions(language):
@@ -24,3 +28,20 @@ def get_answers(question_id, language):
     cur.execute(query)
     answers = cur.fetchall() if cur else []
     return answers
+
+def entry_user_id(sql_query, data):
+    # Проверяем, существует ли уже user_id в таблице
+    sql_check_query = "SELECT login FROM logs WHERE login = %s"
+    cur_log.execute(sql_check_query, data)
+    result = cur_log.fetchone()
+    # Если user_id не существует, то выполняем вставку
+    if not result:
+        try:
+            if data:
+                cur_log.execute(sql_query, data)
+            else:
+                cur_log.execute(sql_query)
+            base_log.commit()
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+
