877ec69b942ea566c2e1aa49321152125c7896bc
Nikita Dubov <dubow1148@yandex.ru>
2024-04-07 18:01:25 +0300

    07.04.24 КОММЕНТИРОВАНИЕ НИКИТА

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
+# Токен для телеграм-бота
 TOKEN = '6427684127:AAH3kWniiI-EA2dBAlLyuKREU76vR8VG4p8'
--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,13 @@
+# Импорт необходимых классов из модуля aiogram
 from aiogram import Bot, Dispatcher
+# Импорт токена бота из файла конфигурации
 from config import TOKEN
+# Импорт класса для хранения состояний FSM в памяти
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+# Создание объекта бота с использованием токена
 bot = Bot(token=TOKEN)
+# Создание объекта для хранения состояний FSM в памяти
 storage = MemoryStorage()
+# Создание диспетчера для обработки запросов от бота с использованием объекта бота и объекта хранения состояний
 dp = Dispatcher(bot, storage=storage)
